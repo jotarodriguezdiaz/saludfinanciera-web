2081ad1078f49faa8f95b6579195b2405f8d7e43
José Carlos Rodríguez Díaz <jotarodriguez@uoc.edu>
2023-04-24 20:34:50 +0200

    ci:configuración entornos desarrollo y producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // urlFinance: 'http://localhost:5054/api/' // IP temporalmetne, después pondré el dominio route 53
+  allowedDomain: 'saludfinanciera.s3-website.eu-west-3.amazonaws.com',
   urlFinance: 'http://saludfinancieragateway.duckdns.org:5003/api/' // IP temporalmetne, después pondré el dominio route 53
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  // urlFinance: 'http://localhost:5054/api/'
-  urlFinance: 'http://saludfinancieragateway.duckdns.org:5003/gateway/' // IP temporalmetne, después pondré el dominio route 53
+  allowedDomain: 'localhost',
+  urlFinance: 'http://localhost:5003/api/'
+  // urlFinance: 'http://saludfinancieragateway.duckdns.org:5003/gateway/' // IP temporalmetne, después pondré el dominio route 53
 };
 
 /*

980e26caf568b5f49c86cfcfb5f39c1818f4618b
José Carlos Rodríguez Díaz <jotarodriguez@uoc.edu>
2023-05-20 21:59:26 +0200

    chore:modificación environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   allowedDomain: 'saludfinanciera.es',
-  urlIdentity: 'http://gateway.saludfinanciera.es:5003/identity',
+  urlIdentity: 'http://gateway.saludfinanciera.es:5003/api/identity',
   urlGateway: 'http://gateway.saludfinanciera.es:5003/gateway'
 };
